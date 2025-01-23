8a6081f69f242ab918c424ff8afbb12a2218ad1c
Brendan-Schaefer-Research <brendan.schaefer.research@gmail.com>
2025-01-23 09:01:27 -0700

    Update linear.js

--- linear.js
+++ linear.js
@@ -1,3 +1,14 @@
+function ONELAY(ARR,weightsarr,biasesarr) {
+
+	let returnarr = matrixmult([ARR],weightsarr)[0];
+	if (addbias == true && biasesarr !== undefined) {
+		returnarr = add2d([returnarr],[biasesarr])[0];
+	}
+
+	return returnarr;
+
+}//takes in 1d array and returns one transform with weights from layer + bias
+
 function LINEAR(input,qlayers,sorted,allweights,allbiases) {
 
 	let nsra = [];
