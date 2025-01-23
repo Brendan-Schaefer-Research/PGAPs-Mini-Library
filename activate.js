7b91545a150e2752e41260e287407d4ebe8fe46e
Brendan-Schaefer-Research <brendan.schaefer.research@gmail.com>
2025-01-23 09:24:17 -0700

    Update activate.js

--- activate.js
+++ activate.js
@@ -1,9 +1,6 @@
 function ACTIVATE(ARR) {
 
-  let ra = [];
-  for (g = 0; g < ARR.length; g++) {
-	  ra[g] = (1/(1+(pow(e,-1*scale*ARR[g]))));
-  }
-  return ra;
+  type = "sigmoid";
+  return activate(ARR);
 	
 }
